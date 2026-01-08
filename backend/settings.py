a4ca6614378ed5e7a366ae48ddec437e1d5b7525
Ivan rono <princeivan733@gmail.com>
2026-01-08 09:36:01 +0300

    host update

--- backend/settings.py
+++ backend/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o0dih+((457@f-h##uicg26#)1z)90ydpuwx72b-#--0ve(2xg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['10.101.100.200']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['10.101.100.200','selector.kalro.org']
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
